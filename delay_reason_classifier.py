83fe7eb331fda156bbc7cb5a91e6fb0978ce47f6
Tomer Greenberg <tgreenberg@lightricks.com>
2020-06-11 13:14:42 +0300

    Added preprocessing in classifier

--- delay_reason_classifier.py
+++ delay_reason_classifier.py
@@ -22,8 +22,22 @@ def parse_hour(time_str):
 #     return x, y
 
 
-def preprocess_data(x, y):
-    pass
+def preprocess_training_data(x, delay, y):
+    """
+    Filters, splits and scales loaded data to be used in training and evaluation. Also saves the scaler.
+    :param x: returned from load_data
+    :param delay: returned from load_data
+    :param y: returned from load_data
+    :return: (x_train, x_val, y_train, y_val)
+    """
+    x, y = x[delay > 0], y[delay > 0]
+    print("Data loaded")
+    x_train, x_val, y_train, y_val = train_test_split(x, y, test_size=0.2, random_state=21)
+    scaler = StandardScaler()
+    x_train = scaler.fit_transform(x_train)
+    x_val = scaler.transform(x_val)
+    joblib.dump(scaler, 'classifier_scaler.pkl')
+    return x_train, x_val, y_train, y_val
 
 
 def train_model(x_train, y_train, max_rows=1000000):
@@ -37,19 +51,15 @@ def train_model(x_train, y_train, max_rows=1000000):
     return classifier
 
 
-def evaluate_model(model, x, y):
-    y_pred = model.predict(x)
-    print(accuracy_score(y, y_pred))
+def evaluate_model(model, x_val, y_val):
+    y_pred = model.predict(x_val)
+    print(accuracy_score(y_val, y_pred))
     print(pd.crosstab(y_val, y_pred, rownames=['Actual Reasons'], colnames=['Predicted Reasons']))
 
 
 if __name__ == '__main__':
     x, delay, y = load_data('flight_data/train_data.csv')
-    print("Data loaded")
-    x_train, x_val, y_train, y_val = train_test_split(x, y, test_size=0.2, random_state=21)
-    scaler = StandardScaler()
-    x_train = scaler.fit_transform(x_train)
-    x_val = scaler.transform(x_val)
+    x_train, x_val, y_train, y_val = preprocess_training_data(x, delay, y)
     classifier = train_model(x, y, max_rows=100000000)
     joblib.dump(classifier, 'classifier.pkl')
     evaluate_model(classifier, x_val, y_val)
